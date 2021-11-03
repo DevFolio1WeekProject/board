596ce4e377250597bb252822cf6557a17b932c3f
wooinjae2 <monsterfairy2@gmail.com>
2021-11-03 22:30:39 +0900

    헤더수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import { Button } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
 import classes from './Header.module.css'
 
-let Header = ()=> {
+let Header = () => {
   const history = useHistory();
-  
+
   let handleWrite = () => {
     console.log('handleWrite');
     history.push('/post/create');
@@ -13,13 +13,13 @@ let Header = ()=> {
   return (
     <header className={classes.header}>
       <div className={classes.header_wrap}>
-        <div>이미지</div>
-        
+        <div><a style={{ cursor: 'pointer' }} onClick={() => { console.log('pyush'); history.push('/') }}><img src="/velog.png" style={{ width: '100px', height: '50px' }}></img></a></div>
+
         <div>
           <Button onClick={handleWrite} primary>글쓰기</Button>
         </div>
       </div>
-      
+
     </header>
   )
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   let [items, setItems] = useState([]);
   useEffect(()=>{
     axios.get('https://limitless-sierra-67996.herokuapp.com/v1/posts?limit=20&page=1').then(res=>{
-      console.log(res);
+      
       setItems(res.data.results);
     })
   },[])
