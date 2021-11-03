77065c723f87c18ebf5380df2ed2353086c965c2
wooinjae2 <monsterfairy2@gmail.com>
2021-11-03 21:27:27 +0900

    semantic ui css 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Home, PostDetailPage, CreatePostPage, EditPostPage } from "./pages";
+import { Container} from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/posts/:id" component={PostDetailPage} />
-      <Route path="/post/create" component={CreatePostPage} />
-      <Route path="/posts/:id/edit" component={EditPostPage} />
-    </div>
+    <Container>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts/:id" component={PostDetailPage} />
+        <Route path="/post/create" component={CreatePostPage} />
+        <Route path="/posts/:id/edit" component={EditPostPage} />
+      </div>
+    </Container>
   );
 }
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,18 @@
+import { Card, Icon, Image } from 'semantic-ui-react';
 import classes from './Item.module.css';
 
 
 export default function Item (props) {
   console.log(props.item);
   return <div className={classes.Item}>
-    <h3>title: {props.item.title}</h3>
-    <div>body: {props.item.body}</div>
     
+    <Card>
+    <Card.Content header={props.item.title} />
+    <Card.Content description={props.item.body} />
+    <Card.Content extra>
+      <Icon name='user' />4 Friends
+    </Card.Content>
+  </Card>
   </div>
 }
 
