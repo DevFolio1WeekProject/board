a59d5c92bd5e909cb513b1d35de67c4c0a75a0cc
ChanGrea <k39335@naver.com>
2021-11-03 23:17:50 +0900

    에디터 ui, 마크다운 에디터(react-md-editor) 추가

--- src/pages/CreatePostPage.js
+++ src/pages/CreatePostPage.js
@@ -1,7 +1,48 @@
 import React from "react";
+import MDEditor from "@uiw/react-md-editor";
+import {
+  Button,
+  Divider,
+  Form,
+  Grid,
+  Input,
+  Icon,
+  TextArea
+} from "semantic-ui-react";
 
 const CreatePostPage = () => {
-  return <div>CreatePostPage</div>;
+  //   const [value, setValue] = React.useState("**Hello world!!!**");
+  return (
+    <Form>
+      <Form.Field>
+        <Input size="big" fluid transparent placeholder="제목을 입력하세요" />
+      </Form.Field>
+      <Form.Field>
+        <Input size="small" fluid transparent placeholder="태그를 입력하세요" />
+      </Form.Field>
+
+      <Divider clearing />
+      {/* <MDEditor value={value} onChange={setValue} /> */}
+      <Form.Field>
+        <TextArea placeholder="Tell us more" />
+      </Form.Field>
+
+      <Divider />
+
+      <Form.Field>
+        <Grid>
+          <Grid.Column floated="left" width={5}>
+            <Icon name="arrow left" />
+            나가기
+          </Grid.Column>
+          <Grid.Column textAlign="right" floated="right" width={5}>
+            <Button secondary>임시저장</Button>
+            <Button primary>출간하기</Button>
+          </Grid.Column>
+        </Grid>
+      </Form.Field>
+    </Form>
+  );
 };
 
 export default CreatePostPage;
