4cea11bebc4a9e6de74b5c66d05089a4dad1b500
wooinjae2 <monsterfairy2@gmail.com>
2021-11-03 21:08:45 +0900

    semantic ui 추가

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,10 @@
-
+import classes from './Item.module.css';
 
 
 export default function Item (props) {
   console.log(props.item);
-  return <div>
-    <div>title: {props.item.title}</div>
+  return <div className={classes.Item}>
+    <h3>title: {props.item.title}</h3>
     <div>body: {props.item.body}</div>
     
   </div>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,14 @@
 import Item from "./Item"
 
 
+
 export default function ItemList(props) {
 
   
   return (
-    props.items.map(item => (<Item key={item.id} item={item}></Item>))
+    <div>
+      {props.items.map(item => (<Item  key={item.id} item={item}></Item>))}
+    </div>
 
   )
 }
