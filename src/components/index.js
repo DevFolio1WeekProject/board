1ac7dd8ee658302e68a1833ec9b97fbfdb9e1acf
ChanGrea <k39335@naver.com>
2021-11-06 13:30:55 +0900

    fixed: build error

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import ItemList from "./ItemList";
 import TagList from "./TagList";
+import ButtonArea from "./ButtonArea";
 
-
-export {
-  Header,
-  TempComponent,
-  ItemList,
-  Footer,
-  TagList
-}
+export { Header, TempComponent, ItemList, Footer, TagList, ButtonArea };
