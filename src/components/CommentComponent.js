6a305d5aed7497b5d71111d8ebd4fd7e1148e85a
ChanGrea <k39335@naver.com>
2021-11-08 17:20:01 +0900

    댓글 목록 css 수정, user icon 추가

--- src/components/CommentComponent.js
+++ src/components/CommentComponent.js
@@ -1,50 +1,47 @@
-import { Comment } from "semantic-ui-react";
-
+import { Comment, Icon } from "semantic-ui-react";
+import { formatDate } from "../utils.js";
 
 let leftPad = (value) => {
-    if (value >= 10) {
-      return value;
-    }
-  
-    return `0${value}`;
-  };
-  
+  if (value >= 10) {
+    return value;
+  }
+
+  return `0${value}`;
+};
+
 let dateFormattedDateString = (date) => {
-//타입스크립트 썻으면 좋았을거 같다.
-let result = "";
-if (!date) {
+  //타입스크립트 썻으면 좋았을거 같다.
+  let result = "";
+  if (!date) {
     return result;
-}
+  }
 
-let year = date.getFullYear();
-let month = leftPad(date.getMonth());
-let day = leftPad(date.getDate());
-let hours = leftPad(date.getHours());
-let minutes = date.getMinutes();
+  let year = date.getFullYear();
+  let month = leftPad(date.getMonth());
+  let day = leftPad(date.getDate());
+  let hours = leftPad(date.getHours());
+  let minutes = date.getMinutes();
 
-result = year + "-" + month + "-" + day + " " + hours + ":" + minutes;
+  result = year + "-" + month + "-" + day + " " + hours + ":" + minutes;
 
-return result;
+  return result;
 };
 
 let CommentComponent = (props) => {
-    console.log('props.comment', props.comment);
-    let comment = props.comment;
+  console.log("props.comment", props.comment);
+  let comment = props.comment;
 
-    let updatedDate = dateFormattedDateString(new Date(props.comment.updatedAt));
-
+  let updatedDate = dateFormattedDateString(new Date(props.comment.updatedAt));
 
   return (
     <Comment>
+      <Comment.Avatar src="/user.png" />
       <Comment.Content>
-        
+        <Comment.Author as="a">Anonymous</Comment.Author>
         <Comment.Metadata>
-          <div>{updatedDate}</div>
+          <div>{formatDate(props.comment.updatedAt)}</div>
         </Comment.Metadata>
         <Comment.Text>{comment.body}</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
       </Comment.Content>
     </Comment>
   );
